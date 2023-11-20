30861e8351129001bb7313fc415b63df9a823ebb
amarixdev <amaridev@alumni.unc.edu>
2023-11-20 10:15:44 -0500

    Initial Commit

---
+++ promoninja_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  promoninja_ios
+//
+//  Created by Amari DeVaughn on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ promoninja_ios/promoninja_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  promoninja_iosApp.swift
+//  promoninja_ios
+//
+//  Created by Amari DeVaughn on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct promoninja_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
